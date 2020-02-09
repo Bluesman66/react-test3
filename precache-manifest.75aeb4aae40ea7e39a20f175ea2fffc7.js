d02ff4e8f25d48c896786a3e54e30a81d19d86be
alex.paclin <alex.paclin@gmail.com>
2020-02-09 12:44:46 +0500

    Updates

---
+++ precache-manifest.75aeb4aae40ea7e39a20f175ea2fffc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7544fb97497fda72ea92a99f5db1b2fc",
+    "url": "/Bluesman66/react-test3.git/index.html"
+  },
+  {
+    "revision": "59310cb5ecaa6c0377e4",
+    "url": "/Bluesman66/react-test3.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2a3904f2ae3d50cdde9b",
+    "url": "/Bluesman66/react-test3.git/static/js/2.2b75c516.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Bluesman66/react-test3.git/static/js/2.2b75c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59310cb5ecaa6c0377e4",
+    "url": "/Bluesman66/react-test3.git/static/js/main.71852ba1.chunk.js"
+  },
+  {
+    "revision": "b3a1d06d3166a041d5de",
+    "url": "/Bluesman66/react-test3.git/static/js/runtime-main.cae832fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Bluesman66/react-test3.git/static/media/logo.5d5d9eef.svg"
+  }
+]);

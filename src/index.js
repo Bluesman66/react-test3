e299ddd7b071638a16aa23392d2fe1354f394625
alex.paclin <alex.paclin@gmail.com>
2020-02-09 15:44:08 +0500

    normalizr

--- src/index.js
+++ src/index.js
@@ -4,20 +4,30 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { apiResponse } from './apiResponse';
 import JSONPretty from 'react-json-pretty';
+import { schema, normalize } from 'normalizr';
 
-const reducer = (state = apiResponse, action) => {
+const user = new schema.Entity('users');
+const comment = new schema.Entity('comments', {
+	author: user
+});
+const lesson = new schema.Entity('lesson', {
+	comments: [comment]
+})
+
+const normalizedData = normalize(apiResponse, lesson);
+
+const reducer = (state = normalizedData.entities, action) => {
 	if (action.type === 'LIKE_COMMENT') {
-		let comments = state.comments.slice(0);
-
-		comments = comments.map(c => {
-			const newComment = Object.assign({}, c);
-			if (newComment.id === action.commentId) {
-				newComment.likes += 1;
+		return {
+			...state,
+			comments: {
+				...state.comments,
+				[action.commentId]: {
+					...state.comments[action.commentId],
+					likes: state.comments[action.commentId].likes + 1
+				}
 			}
-			return newComment;
-		})
-
-		return Object.assign({}, state, { comments });
+		}
 	}
 	return state;
 }

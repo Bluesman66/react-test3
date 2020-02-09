04be3111ada883e43d8fb17f36805ca6718873c2
alex.paclin <alex.paclin@gmail.com>
2020-02-09 13:49:35 +0500

    JSONPretty

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { apiResponse } from './apiResponse';
+import JSONPretty from 'react-json-pretty';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state = apiResponse, action) => {
+	return state;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducer);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<div>
+			<JSONPretty json={store.getState()} />
+		</div>
+	</Provider>,
+	document.getElementById('root'));

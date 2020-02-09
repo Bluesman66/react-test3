e6f859cdff6c45645b3b1ee159cf44497c5b9084
alex.paclin <alex.paclin@gmail.com>
2020-02-09 14:34:08 +0500

    comments.map

--- src/index.js
+++ src/index.js
@@ -6,15 +6,33 @@ import { apiResponse } from './apiResponse';
 import JSONPretty from 'react-json-pretty';
 
 const reducer = (state = apiResponse, action) => {
+	if (action.type === 'LIKE_COMMENT') {
+		let comments = state.comments.slice(0);
+
+		comments = comments.map(c => {
+			const newComment = Object.assign({}, c);
+			if (newComment.id === action.commentId) {
+				newComment.likes += 1;
+			}
+			return newComment;
+		})
+
+		return Object.assign({}, state, { comments });
+	}
 	return state;
 }
 
 const store = createStore(reducer);
+window.store = store;
 
-ReactDOM.render(
-	<Provider store={store}>
-		<div>
-			<JSONPretty json={store.getState()} />
-		</div>
-	</Provider>,
-	document.getElementById('root'));
+const renderApp = () => {
+	ReactDOM.render(
+		<Provider store={store}>
+			<div>
+				<JSONPretty json={store.getState()} />
+			</div>
+		</Provider>,
+		document.getElementById('root'));
+}
+
+store.subscribe(renderApp);
